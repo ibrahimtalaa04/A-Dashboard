658c0c62ed15833d17e41bdd5aae2f5bfea6f410
IBRAHIM ALTALAA <ibrahim6tal@gmail.com>
2022-08-24 20:54:29 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const body=document.querySelector("body"),
+    modeToggle=body.querySelector(".mode-toggle"),
+    sidebar=body.querySelector("nav"),
+    sideparToggle=body.querySelector(".sidepar-toggle");
+
+    let getMode=localStorage.getItem("mode");
+    if(getMode&& getMode === "dark"){
+        body.classList.toggle("dark");
+    }
+    let getStatus=localStorage.getItem("status");
+    if(getStatus && getStatus === "close"){
+        sidebar.classList.toggle("close");
+    }
+
+modeToggle.addEventListener("click",()=>{
+    body.classList.toggle("dark"); 
+    if(body.classList.contains("dark"))
+    {
+        localStorage.setItem("mode","dark");
+    } else localStorage.setItem("mode","light");
+});
+sideparToggle.addEventListener("click",()=>{
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close"))
+        localStorage.setItem("status","close");
+    else localStorage.setItem("status","open");
+});
